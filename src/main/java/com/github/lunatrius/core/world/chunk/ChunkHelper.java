734815eadb5a5869ae671ab2e5a70de5204aaf55
asquared31415 <34665709+asquared31415@users.noreply.github.com>
2024-11-04 12:11:56 -0500

    fix slime chunks algorithm

--- src/main/java/com/github/lunatrius/core/world/chunk/ChunkHelper.java
+++ src/main/java/com/github/lunatrius/core/world/chunk/ChunkHelper.java
@@ -7,7 +7,9 @@ public class ChunkHelper {
     private static final Random RANDOM = new Random();
 
     public static boolean isSlimeChunk(long seed, int x, int z) {
-        RANDOM.setSeed(seed + (x * x * 4987142) + (x * 5947611) + (z * z * 4392871) + (z * 389711) ^ 987234911);
+        RANDOM.setSeed(
+            seed + (int) (x * x * 0x4c1906) + (int) (x * 0x5ac0db) + (int) (z * z) * 0x4307a7L + (int) (z * 0x5f24f)
+                ^ 0x3ad8025fL);
         return RANDOM.nextInt(10) == 0;
     }
 }
